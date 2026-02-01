b78f2374f961faad7492bd9656606533824b872d
the-bumbler <etroneacc@gmail.com>
2026-02-01 06:50:40 -0800

    Add files via upload

--- decode.v
+++ decode.v
@@ -11,7 +11,7 @@ module decode #(
     // PC & PC+4 input and output 
     input      [DATA_WIDTH-1:0] PCB,
     input      [DATA_WIDTH-1:0] PCPlus4B,
-    output     [DATA_WIDTH-1:0] PCTargetA,
+    output reg [DATA_WIDTH-1:0] PCC,
     output reg [DATA_WIDTH-1:0] PCPlus4C,
     // Register write-back data, address, and signal (from E stage)
     input                       WrEnE,
@@ -19,12 +19,13 @@ module decode #(
     input      [DATA_WIDTH-1:0] WrDataE,
     // Control Signal outputs 
     output reg                  RegWriteC,
-    output reg [1:0]            ResultSrcC,
     output reg                  MemWriteC,
     output reg                  JumpC,
     output reg                  BranchC,
     output reg [1:0]            ALUSrcC,
+    output reg [1:0]            ResultSrcC,
     output reg [1:0]            ALUOpC,
+    output reg                  LinkRegCtrlC,
     // Immediate extended
     output reg [DATA_WIDTH-1:0] ImmExtC,
     // Destination address
@@ -53,11 +54,23 @@ wire RegWrite;
 wire MemWrite;
 wire Jump;
 wire Branch;
-wire [1:0] ALUSrc;    // ALUSrc[0] will control the mux for rs2 & imm, ALUSrc[1] will control the mux for rs1 & PC
-wire [1:0] ResultSrc; // control signal for 4 input mux at writeback stage
+wire [1:0] ALUSrc;    // ALUSrc[0] will control the mux for rs2 & imm (B input), ALUSrc[1] will control the mux for rs1 & PC (A input)
+wire [1:0] ResultSrc; // control signal for 4 input mux at writeback stage (00=ALU result, 01=memory, 10=PC+4, 11=Imm)
+wire [1:0] ALUOp;     // ALU control (00=do nothing, 01=accept funct, 10=add, 11=sub)
+wire LinkRegCtrl;     // control for Jalr mux (pass Rs1 to PC+Imm module instead of PC)
 wire [2:0] ImmGenSrc; // immediate generator signals for 5 types of immediate formats
-wire [1:0] ALUOp;     // ALU control that chooses functs, or forces add/sub
-
+/*
+// Control Unit reg
+reg RegWrite;
+reg MemWrite;
+reg Jump;
+reg Branch;
+reg [1:0] ALUSrc;    // ALUSrc[0] will control the mux for rs2 & imm (B input), ALUSrc[1] will control the mux for rs1 & PC (A input)
+reg [1:0] ResultSrc; // control signal for 4 input mux at writeback stage (00=ALU result, 01=memory, 10=PC+4, 11=Imm)
+reg [1:0] ALUOp;     // ALU control (00=do nothing, 01=accept funct, 10=add, 11=sub)
+reg LinkRegCtrl;     // control for Jalr mux (pass Rs1 to PC+Imm module instead of PC)
+reg [2:0] ImmGenSrc; // immediate generator signals for 5 types of immediate formats
+*/
 // Opcode parameters
 localparam OP_LOAD   = 7'b0000011;
 localparam OP_STORE  = 7'b0100011;
@@ -70,6 +83,7 @@ localparam OP_LUI    = 7'b0110111;
 localparam OP_AUIPC  = 7'b0010111;
 
 // Immediate type signals
+localparam NO_IMM = 3'b000;
 localparam I_IMM = 3'b001;
 localparam S_IMM = 3'b010;
 localparam B_IMM = 3'b011;
@@ -77,7 +91,7 @@ localparam U_IMM = 3'b100;
 localparam J_IMM = 3'b101;
 
 // Immediate Extender Result
-wire ImmExt;
+wire [DATA_WIDTH-1:0] ImmExt;
 
 // Immediate Extender 
 //      For I-type instructions, Imm[11:0] are Instr[31:20]
@@ -87,22 +101,111 @@ assign ImmExt = (ImmGenSrc == I_IMM) ? {{(DATA_WIDTH-12){InstrB[31]}}, InstrB[31
                 (ImmGenSrc == S_IMM) ? {{(DATA_WIDTH-12){InstrB[31]}}, InstrB[31:25], InstrB[11:7]} : 
                 (ImmGenSrc == B_IMM) ? {{(DATA_WIDTH-12){InstrB[31]}}, InstrB[7], InstrB[30:25], InstrB[11:8], 1'b0} : 
                 (ImmGenSrc == U_IMM) ? {{(DATA_WIDTH-32){InstrB[31]}}, InstrB[31:12], {12{1'b0}}} :
-                (ImmGenSrc == J_IMM) ? {{(DATA_WIDTH-20){InstrB[31]}}, InstrB[31], InstrB[19:12], InstrB[20], InstrB[30:21], 1'b0} : 
+                (ImmGenSrc == J_IMM) ? {{(DATA_WIDTH-20){InstrB[31]}}, InstrB[19:12], InstrB[20], InstrB[30:21], 1'b0} : 
                 32'h0000_0000;
 
-/*
 // Control Unit 
-assign RegWrite  = (Opcode == OP_LOAD  | Opcode == OP_REG  | Opcode == OP_IMM | Opcode ==  OP_JUMP | Opcode == OP_LUI | Opcode == OP_AUIPC) ? 1'b1 : 1'b0;
-assign MemWrite  = (Opcode == OP_STORE) ? 1'b1 : 1'b0; 
-assign Jump      = (Opcode == OP_JUMP) ? 1'b1 : 1'b0;
-assign Branch    = (Opcode == OP_BRANCH) ? 1'b1 : 1'b0;   
-assign ALUSrc    = (Opcode == OP_LOAD | Opcode == OP_STORE | Opcode == OP_IMM | ) ? 1'b1 : 1'b0;
-//assign ALUSrc[1] = (Opcode == OP_JALR) ? 1'b1 : 1'b0;
-assign ALUOp     = (Opcode == OP_JAL | Opcode == OP_JALR) ? 2'b01 : (Opcode == OP_LUI) ? 2'b10 : (Opcode == OP_AUIPC) ? 2'b11 : 2'b00;
-assign ResultSrc = (Opcode == OP_LOAD) ? 1'b01 : (Opcode == OP_JUMP) ? 1'b10 : 1'b00; 
+assign RegWrite  = (Opcode == OP_LOAD  | Opcode == OP_REG  | Opcode == OP_IMM | Opcode ==  OP_JAL | Opcode ==  OP_JALR | Opcode == OP_LUI | Opcode == OP_AUIPC) ? 1'b1 : 1'b0;
+assign MemWrite = (Opcode == OP_STORE) ? 1'b1 : 1'b0;
+assign Jump = (Opcode == OP_JAL | Opcode == OP_JALR) ? 1'b1 : 1'b0;
+assign Branch = (Opcode == OP_BRANCH) ? 1'b1 : 1'b0;
+assign ALUSrc[0] = (Opcode == OP_LOAD | Opcode == OP_STORE | Opcode == OP_IMM | Opcode == OP_AUIPC) ? 1'b1 : 1'b0;
+assign ALUSrc[1] = (Opcode == OP_AUIPC) ? 1'b1 : 1'b0;
+assign ResultSrc = (Opcode == OP_LOAD) ? 2'b01 : (Opcode == OP_JAL | Opcode == OP_JALR) ? 2'b10 : (Opcode == OP_LUI) ? 2'b11 : 2'b00;
+assign ALUOp = (Opcode == OP_REG | Opcode == OP_IMM) ? 2'b01 : (Opcode == OP_LOAD | Opcode == OP_STORE | Opcode == OP_AUIPC) ? 2'b10 : (Opcode == OP_BRANCH) ? 2'b11 : 2'b00;
+assign LinkRegCtrl = (Opcode == OP_JALR) ? 1'b1 : 1'b0;
 assign ImmGenSrc = (Opcode == OP_IMM) ? I_IMM : (Opcode == OP_STORE) ? S_IMM : (Opcode == OP_BRANCH) ? B_IMM : 
-                   (Opcode == OP_JALR | Opcode == OP_JAL) ? J_IMM : (Opcode == OP_AUIPC | Opcode == OP_LUI) ? U_IMM : 3'b000;
+                   (Opcode == OP_AUIPC | Opcode == OP_LUI) ? U_IMM : (Opcode == OP_JAL | Opcode == OP_JALR) ? J_IMM : NO_IMM;
+
+/*
+wire if_LOAD   = (Opcode == OP_LOAD);
+wire if_STORE  = (Opcode == OP_STORE);
+wire if_BRANCH = (Opcode == OP_BRANCH);
+wire if_REG    = (Opcode == OP_REG);
+wire if_IMM    = (Opcode == OP_IMM);
+wire if_JAL    = (Opcode == OP_JAL);
+wire if_JALR   = (Opcode == OP_JALR);
+wire if_LUI    = (Opcode == OP_LUI);
+wire if_AUIPC  = (Opcode == OP_AUIPC);
+
+always @(*) begin
+    RegWrite = if_LOAD | if_REG | if_IMM | if_JAL | if_JALR | if_LUI | if_AUIPC;
+    MemWrite = if_STORE;
+    Jump = if_JAL | if_JALR;
+    Branch = if_BRANCH;
+    ALUSrc[0] = if_LOAD | if_STORE | if_IMM | if_AUIPC;
+    ALUSrc[1] = if_AUIPC;
+    // Result Source
+    if(if_REG | if_IMM | if_AUIPC | if_BRANCH | if_STORE) ResultSrc = 2'b00; // branch and store are included here because it doesn't matter what they are
+    else if(if_LOAD) ResultSrc = 2'b01;
+    else if(if_JAL | if_JALR) ResultSrc = 2'b10;
+    else if(if_LUI) ResultSrc = 2'b11;
+    // ALU Operation
+    if(if_LUI | if_JAL | if_JALR) ALUOp = 2'b00;
+    else if(if_REG | if_IMM) ALUOp = 2'b01;
+    else if(if_LOAD | if_STORE | if_AUIPC) ALUOp = 2'b10;
+    else if(if_BRANCH) ALUOp = 2'b11;
+    LinkRegCtrl = if_JALR;
+end
 */
+// Register File 
+reg [DATA_WIDTH-1:0] RegisterFile [31:0];
 
+integer i;
 
+// Register Assignment
+always @(posedge clk or negedge rst_n) begin
+    if(~rst_n) begin
+        RegWriteC <= 0;
+        ResultSrcC <= 0;
+        MemWriteC <= 0;
+        JumpC <= 0;
+        BranchC <= 0;
+        ALUSrcC <= 0;
+        ALUOpC <= 0;
+        LinkRegCtrlC <= 0;
+        ImmExtC <= 0;
+        RdC <= 0;
+        RData1C <= 0;
+        RData2C <= 0;
+        Funct7C <= 0;
+        Funct3C <= 0;
+        Rs1C <= 0;
+        Rs2C <= 0;
+        PCC <= 0;
+        PCPlus4C <= 0;
+        for(i = 0; i < 32; i = i + 1) begin
+            RegisterFile[i] <= 0;
+        end
+    end else begin
+        // Register write
+        if(WrEnE) begin
+            RegisterFile[WrAddrE] <= WrDataE;
+        end
+        // Register read
+        RData1C <= RegisterFile[Rs1];
+        RData2C <= RegisterFile[Rs2];
+        // Control signal assignment
+        RegWriteC <= RegWrite;
+        ResultSrcC <= ResultSrc;
+        MemWriteC <= MemWrite;
+        JumpC <= Jump;
+        BranchC <= Branch;
+        ALUSrcC <= ALUSrc;
+        ALUOpC <= ALUOp;
+        LinkRegCtrlC <= LinkRegCtrl;
+        // Immediate extension
+        ImmExtC <= ImmExt;
+        // Rd, rs1, rs2 addr
+        RdC <= Rd;
+        Rs1C <= Rs1;
+        Rs2C <= Rs2;
+        // Funct7 & Funct3
+        Funct7C <= Funct7;
+        Funct3C <= Funct3;
+        // PC & PC+4
+        PCC <= PCB;
+        PCPlus4C <= PCPlus4B;
+    end
+end
 endmodule

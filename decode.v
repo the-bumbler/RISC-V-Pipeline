f8ad6a65111670f2cc80a0f0cffb85898b65116e
the-bumbler <etroneacc@gmail.com>
2026-02-21 16:21:06 -0800

    Fixed Register File
    Register file to be negative edge triggered

--- decode.v
+++ decode.v
@@ -26,6 +26,7 @@ module decode #(
     output reg [1:0]            ResultSrcC,
     output reg [1:0]            ALUOpC,
     output reg                  LinkRegCtrlC,
+    output reg [4:0]            ALUControlC,
     // Immediate extended
     output reg [DATA_WIDTH-1:0] ImmExtC,
     // Destination address
@@ -34,7 +35,7 @@ module decode #(
     output reg [DATA_WIDTH-1:0] RData1C,
     output reg [DATA_WIDTH-1:0] RData2C,
     // Function 7 & 3 
-    output reg [6:0]            Funct7C,
+    //output reg [6:0]            Funct7C,
     output reg [2:0]            Funct3C,
     // Rs Hazard control address 
     output reg [4:0]            Rs1C,
@@ -59,18 +60,8 @@ wire [1:0] ResultSrc; // control signal for 4 input mux at writeback stage (00=A
 wire [1:0] ALUOp;     // ALU control (00=do nothing, 01=accept funct, 10=add, 11=sub)
 wire LinkRegCtrl;     // control for Jalr mux (pass Rs1 to PC+Imm module instead of PC)
 wire [2:0] ImmGenSrc; // immediate generator signals for 5 types of immediate formats
-/*
-// Control Unit reg
-reg RegWrite;
-reg MemWrite;
-reg Jump;
-reg Branch;
-reg [1:0] ALUSrc;    // ALUSrc[0] will control the mux for rs2 & imm (B input), ALUSrc[1] will control the mux for rs1 & PC (A input)
-reg [1:0] ResultSrc; // control signal for 4 input mux at writeback stage (00=ALU result, 01=memory, 10=PC+4, 11=Imm)
-reg [1:0] ALUOp;     // ALU control (00=do nothing, 01=accept funct, 10=add, 11=sub)
-reg LinkRegCtrl;     // control for Jalr mux (pass Rs1 to PC+Imm module instead of PC)
-reg [2:0] ImmGenSrc; // immediate generator signals for 5 types of immediate formats
-*/
+wire [4:0] ALUControl;
+
 // Opcode parameters
 localparam OP_LOAD   = 7'b0000011;
 localparam OP_STORE  = 7'b0100011;
@@ -117,43 +108,30 @@ assign LinkRegCtrl = (Opcode == OP_JALR) ? 1'b1 : 1'b0;
 assign ImmGenSrc = (Opcode == OP_IMM) ? I_IMM : (Opcode == OP_STORE) ? S_IMM : (Opcode == OP_BRANCH) ? B_IMM : 
                    (Opcode == OP_AUIPC | Opcode == OP_LUI) ? U_IMM : (Opcode == OP_JAL | Opcode == OP_JALR) ? J_IMM : NO_IMM;
 
-/*
-wire if_LOAD   = (Opcode == OP_LOAD);
-wire if_STORE  = (Opcode == OP_STORE);
-wire if_BRANCH = (Opcode == OP_BRANCH);
-wire if_REG    = (Opcode == OP_REG);
-wire if_IMM    = (Opcode == OP_IMM);
-wire if_JAL    = (Opcode == OP_JAL);
-wire if_JALR   = (Opcode == OP_JALR);
-wire if_LUI    = (Opcode == OP_LUI);
-wire if_AUIPC  = (Opcode == OP_AUIPC);
+// ALU Control
+assign ALUControl[2:0] = Funct3;                        // first 3 bits are funct3
+assign ALUControl[3] = Funct7[5];                       // 4th bit is for the add/sub & srl/sra control
+assign ALUControl[4] = (ALUSrc[0]) ? 1'b0 : Funct7[0];  // 5th bit is for integer multiplication extension, which uses the ALU
 
-always @(*) begin
-    RegWrite = if_LOAD | if_REG | if_IMM | if_JAL | if_JALR | if_LUI | if_AUIPC;
-    MemWrite = if_STORE;
-    Jump = if_JAL | if_JALR;
-    Branch = if_BRANCH;
-    ALUSrc[0] = if_LOAD | if_STORE | if_IMM | if_AUIPC;
-    ALUSrc[1] = if_AUIPC;
-    // Result Source
-    if(if_REG | if_IMM | if_AUIPC | if_BRANCH | if_STORE) ResultSrc = 2'b00; // branch and store are included here because it doesn't matter what they are
-    else if(if_LOAD) ResultSrc = 2'b01;
-    else if(if_JAL | if_JALR) ResultSrc = 2'b10;
-    else if(if_LUI) ResultSrc = 2'b11;
-    // ALU Operation
-    if(if_LUI | if_JAL | if_JALR) ALUOp = 2'b00;
-    else if(if_REG | if_IMM) ALUOp = 2'b01;
-    else if(if_LOAD | if_STORE | if_AUIPC) ALUOp = 2'b10;
-    else if(if_BRANCH) ALUOp = 2'b11;
-    LinkRegCtrl = if_JALR;
-end
-*/
 // Register File 
 reg [DATA_WIDTH-1:0] RegisterFile [31:0];
 
 integer i;
 
-// Register Assignment
+// Register File
+always @(negedge clk or negedge rst_n) begin
+    if(rst_n) begin
+        for(i = 0; i < 32; i = i + 1) begin
+            RegisterFile[i] <= 0;
+        end
+    end else begin
+        if(WrEnE) begin
+            RegisterFile[WrAddrE] <= WrDataE;
+        end
+    end
+end
+
+// C side output Assignment
 always @(posedge clk or negedge rst_n) begin
     if(~rst_n) begin
         RegWriteC <= 0;
@@ -164,6 +142,7 @@ always @(posedge clk or negedge rst_n) begin
         ALUSrcC <= 0;
         ALUOpC <= 0;
         LinkRegCtrlC <= 0;
+        ALUControlC <= 0;
         ImmExtC <= 0;
         RdC <= 0;
         RData1C <= 0;
@@ -174,14 +153,7 @@ always @(posedge clk or negedge rst_n) begin
         Rs2C <= 0;
         PCC <= 0;
         PCPlus4C <= 0;
-        for(i = 0; i < 32; i = i + 1) begin
-            RegisterFile[i] <= 0;
-        end
     end else begin
-        // Register write
-        if(WrEnE) begin
-            RegisterFile[WrAddrE] <= WrDataE;
-        end
         // Register read
         RData1C <= RegisterFile[Rs1];
         RData2C <= RegisterFile[Rs2];
@@ -194,6 +166,7 @@ always @(posedge clk or negedge rst_n) begin
         ALUSrcC <= ALUSrc;
         ALUOpC <= ALUOp;
         LinkRegCtrlC <= LinkRegCtrl;
+        ALUControlC <= ALUControl;
         // Immediate extension
         ImmExtC <= ImmExt;
         // Rd, rs1, rs2 addr
@@ -201,7 +174,7 @@ always @(posedge clk or negedge rst_n) begin
         Rs1C <= Rs1;
         Rs2C <= Rs2;
         // Funct7 & Funct3
-        Funct7C <= Funct7;
+        //Funct7C <= Funct7;
         Funct3C <= Funct3;
         // PC & PC+4
         PCC <= PCB;
